4eae198232c93864f530c10942863a9f48a79b24
shivako <kshivapad@yahoo.com>
2022-01-18 15:34:43 -0500

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const socket = require("socket.io");
 const app = express();
 
 var server = app.listen(4000, function(){
@@ -18,11 +19,40 @@ io.on("connection", function(socket){
         console.log("rooms: "+ rooms)
         var room = rooms.get(roomName);
         if (room  ==  undefined){
+            console.log("Room not found: "+ roomName)
             socket.join(roomName)
+            socket.emit("created")
         }else if (room.size ==1 ){
             socket.join(roomName)
+            socket.emit("joined")
         }else {
+            socket.emit("full")
             console.log("Room is full for now: "+ roomName)
         }
     });
+
+    socket.on("ready", function(roomName){
+        console.log("Ready event ")
+        socket.broadcast.to(roomName).emit("ready")
+    }
+    );
+
+    socket.on("candidate", function(candidate, roomName ){
+        console.log("Candidate event ")
+        socket.broadcast.to(roomName).emit("candidate", candidate);
+    }
+    );
+
+    socket.on("offer", function(offer, roomName ){
+        console.log("Offer event ")
+        socket.broadcast.to(roomName).emit("offer", offer);
+    }
+    );
+
+    socket.on("answer", function(answer, roomName ){
+        console.log("Answer event ")
+        socket.broadcast.to(roomName).emit("answer", answer);
+    }
+    );
+
 });
--- public/chat.js
+++ public/chat.js
@@ -1,38 +1,90 @@
 var socket = io.connect("")
 var divVideoChatLobby = document.getElementById("video-chat-lobby")
-var roomName = document.getElementById("roomName")
+var roomNameHolder = document.getElementById("roomName")
 var joinButton = document.getElementById("join")
 var divVideoChatRoom = document.getElementById("video-chat-room")
 var userVideo = document.getElementById("user-video")
 var peerVideo = document.getElementById("peer-video")
+var roomName = roomNameHolder.value
+
+var creator = false;
 
 joinButton.addEventListener('click', function(){
-    if (roomName.value == ""){
+    if (roomName == ""){
         alert("Please enter Room Name")
     }else{
-        socket.emit("join", roomName.value)
-        console.log("about to getUserMedia")
-        navigator.getUserMedia(
-        {
-                audio: false,
-                video: { width: 1280, height: 720 }
-        }    
-        , 
-        
-        function(stream){
-            divVideoChatLobby.style ="display:none"
-            userVideo.srcObject = stream ;
-            userVideo.onloadedmetadata = function(e){
-                userVideo.play();
-            }
-
-        }, 
-        
-        function(err){
-            console.log("could not access usermedia", err)
-            alert("could not access usermedia", err)
-        });
+        socket.emit("join", roomName)
+ 
     }
 }
 );
 
+socket.on("created", function(){
+    creator = true;
+    console.log("about to getUserMedia");
+    navigator.getUserMedia(
+    {
+            audio: false,
+            video: { width: 1280, height: 720 }
+    }    
+    , 
+    
+    function(stream){
+        divVideoChatLobby.style ="display:none"
+        userVideo.srcObject = stream ;
+        userVideo.onloadedmetadata = function(e){
+            userVideo.play();
+        }
+
+    }, 
+    
+    function(err){
+        console.log("could not access usermedia", err)
+        alert("could not access usermedia", err)
+    });
+});
+
+socket.on("joined", function(){
+    creator = false;
+    console.log("about to getUserMedia");
+    navigator.getUserMedia(
+    {
+            audio: false,
+            video: { width: 1280, height: 720 }
+    }    
+    , 
+    
+    function(stream){
+        divVideoChatLobby.style ="display:none"
+        userVideo.srcObject = stream ;
+        userVideo.onloadedmetadata = function(e){
+            userVideo.play();
+        }
+
+    }, 
+    
+    function(err){
+        console.log("could not access usermedia", err)
+        alert("could not access usermedia", err)
+    });
+});
+
+socket.on("full", function(){
+    alert("Room is full, Can't join");
+});
+
+socket.on("ready", function(){
+
+});
+
+socket.on("candidate", function(){
+
+});
+
+socket.on("offer", function(){
+
+});
+
+socket.on("answer", function(){
+
+});
